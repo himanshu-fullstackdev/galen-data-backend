b7f1098eb93b611f5542a63f99f7f10d7bc30946
himanshu <himanshu73188@gmail.com>
2021-07-10 14:58:42 +0530

    added the route to fetch all events stored in the db

---
+++ controllers/v1/event.js
@@ -0,0 +1,56 @@
+//Error code - 1000
+
+// import modules
+const async = require("async");
+
+// import models
+const Website = require("../../src/models/v1/website");
+const Event = require("../../src/models/v1/event");
+
+exports.fetchAllEvents = async function (req, res, next) {
+  const eventsData = [];
+
+  try {
+    // fetch all websites from the website table
+    const websites = await Website.findAll();
+
+    // loop through the fetched websites array
+    async.forEachOf(
+      websites,
+      async function (website, index) {
+        // fetch all events from the event table w.r.t website id
+        const events = await Event.findAll({
+          where: { websiteId: website.id },
+        });
+
+        // push data to the eventsData array
+        eventsData.push({
+          id: website.id,
+          website: website.website,
+          scrapeUrl: website.scrapeUrl,
+          createdAt: createdAt,
+          updatedAt: updatedAt,
+          events: events,
+        });
+      },
+      (err) => {
+        if (err) {
+          // return error
+          const error = {
+            status: "failure",
+            message: "Error: Please do it again",
+            code: "1000x0001",
+            error: err,
+          };
+          return res.status(500).send(error);
+        } else {
+          // return success response
+          return res.status(200).send({ status: "success", data: eventsData });
+        }
+      }
+    );
+  } catch (err) {
+    // return error
+    return res.status(500).send({ status: "failure", code: "1000x0002" });
+  }
+};
--- routes/v1/event.js
+++ routes/v1/event.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// controllers
+const eventController = require("../../controllers/v1/event");
+
+// GET | /events | fetch all events
+// -------------------------------------------
+router.get("/", eventController.fetchAllEvents);
+
 module.exports = router;
