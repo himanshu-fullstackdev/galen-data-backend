ca2aa10083b6dc1dc6f06b724700ddec1addb678
himanshu <himanshu73188@gmail.com>
2021-07-10 17:39:49 +0530

    added a feature to run scrapeWebsites method if events table is empty & also scheduled a cron job

--- controllers/v1/event.js
+++ controllers/v1/event.js
@@ -2,6 +2,7 @@
 
 // import modules
 const async = require("async");
+const cron = require("node-cron");
 
 // import models
 const Website = require("../../src/models/v1/website");
@@ -15,43 +16,56 @@ exports.fetchAllEvents = async function (req, res, next) {
     const websites = await Website.findAll();
 
     // loop through the fetched websites array
-    async.forEachOf(
-      websites,
-      async function (website, index) {
-        // fetch all events from the event table w.r.t website id
-        const events = await Event.findAll({
-          where: { websiteId: website.id },
-          attributes: {
-            exclude: ["createdAt", "updatedAt"],
-          },
-        });
+    if (websites.length > 0) {
+      async.forEachOf(
+        websites,
+        async function (website, index) {
+          // fetch all events from the event table w.r.t website id
+          const events = await Event.findAll({
+            where: { websiteId: website.id },
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          });
 
-        // push data to the eventsData array
-        eventsData.push({
-          id: website.id,
-          website: website.website,
-          scrapeUrl: website.scrapeUrl,
-          events: events,
-        });
-      },
-      (err) => {
-        if (err) {
-          // return error
-          const error = {
-            status: "failure",
-            message: "Error: Please do it again",
-            code: "1000x0001",
-            error: err,
-          };
-          return res.status(500).send(error);
-        } else {
-          // return success response
-          return res.status(200).send({ status: "success", data: eventsData });
+          // push data to the eventsData array
+          eventsData.push({
+            id: website.id,
+            website: website.website,
+            scrapeUrl: website.scrapeUrl,
+            events: events,
+          });
+        },
+        (err) => {
+          if (err) {
+            // return error
+            const error = {
+              status: "failure",
+              message: "Error: Please do it again",
+              code: "1000x0001",
+              error: err,
+            };
+            return res.status(500).send(error);
+          } else {
+            // return success response
+            return res
+              .status(200)
+              .send({ status: "success", data: eventsData });
+          }
         }
-      }
-    );
+      );
+    } else {
+      return res.status(200).send({ status: "success", data: [] });
+    }
   } catch (err) {
     // return error
     return res.status(500).send({ status: "failure", code: "1000x0002" });
   }
 };
+
+exports.scrapeWebsites = async function (req, res, next) {};
+
+// Run scrapeWebsites after every 24 hours.
+cron.schedule("0 0 0 * * *", () => {
+  scrapeWebsites();
+});
--- src/models/v1/event.js
+++ src/models/v1/event.js
@@ -2,25 +2,45 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../../../config/db_credentials");
 
-const Event = sequelize.define("event", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+// controllers
+const eventController = require("../../../controllers/v1/event");
+
+const Event = sequelize.define(
+  "event",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    location: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  location: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      afterSync: async function (options) {
+        try {
+          const events = await Event.findAll();
+          if (events.length == 0) {
+            eventController.scrapeWebsites();
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    },
+  }
+);
 
 module.exports = Event;
