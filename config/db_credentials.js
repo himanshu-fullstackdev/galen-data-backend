1471b5063c1aa9c545b260b49f9374e126b7cf32
himanshu <himanshu73188@gmail.com>
2021-07-10 18:01:05 +0530

    added the initial data in website model

--- config/db_credentials.js
+++ config/db_credentials.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("galen_data", "root", "", {
   dialect: "mysql",
   host: "localhost",
+  logging: false,
 });
 
 module.exports = sequelize;
--- src/models/v1/website.js
+++ src/models/v1/website.js
@@ -2,21 +2,46 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../../../config/db_credentials");
 
-const Website = sequelize.define("website", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Website = sequelize.define(
+  "website",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    website: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    scrapeUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  website: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  scrapeUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      afterSync: async function (options) {
+        try {
+          const websites = await Website.findAll();
+          if (websites.length == 0) {
+            Website.create({
+              website: "https://www.computerworld.com",
+              scrapeUrl:
+                "https://www.computerworld.com/article/3313417/tech-event-calendar-2020-upcoming-shows-conferences-and-it-expos.html",
+            });
+            Website.create({
+              website: "https://www.techmeme.com",
+              scrapeUrl: "https://www.techmeme.com/events",
+            });
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    },
+  }
+);
 
 module.exports = Website;
